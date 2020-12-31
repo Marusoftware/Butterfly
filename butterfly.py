147c66d92a5215473e0a389cce40a900d6122a71
Marusoftware <marusoftware@outlook.jp>
2020-12-31 18:26:06 +0900

    init

---
+++ butterfly.py
@@ -0,0 +1,47 @@
+try:
+    import tkinter
+    from tkinter import ttk
+except:
+    import Tkinter as tkinter
+    import ttk
+import os
+
+##Default Settings
+conf = {
+}
+
+class libs():
+    def getDir(module_name):
+        os.path.join("",module_name)
+    class Config():
+        def __init__(self):
+            self.config = open()
+
+class TUI_CUR():
+    def __init__(self):
+        pass
+
+class TUI_CUI():
+    def __init__(self):
+        pass
+
+class CUI_CMD():
+    def __init__(self):
+        pass
+
+class CUI_SHELL():
+    def __init__(self):
+        pass
+
+class GUI():
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.title("Marusoftware - Butterfly")
+
+class CORE():
+    def __init__(self):
+        pass
+    class Python2():
+        pass
+    class Python3():
+        pass
