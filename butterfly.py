715441a6c71b2533ba94c6b5ff5aacb1f9192f9a
Marusoftware <marusoftware@outlook.jp>
2021-01-14 21:23:47 +0900

    ...oops...

--- butterfly.py
+++ butterfly.py
@@ -1,21 +1,12 @@
-try:
-    import tkinter
-    from tkinter import ttk
-except:
-    import Tkinter as tkinter
-    import ttk
-import os, argparse, sys
+import os, sys
+import argparse, configparser
+import libbutterfly
+
+__version__ = "beta0.0.1-pre"
 
 ##Default Settings
 conf = {
 }
-
-class libs():
-    def get_module_Dir(module_name):
-        os.path.join("",module_name)
-    class Config():
-        def __init__(self):
-            self.config = open()
 
 class UI():
     class TUI_CUR():
@@ -24,7 +15,7 @@ class UI():
 
     class TUI_CUI():
         def __init__(self):
-            if input("!WARN! This UI outputs is very long! Do you want to continue? (y or n)") == "y":
+            if input("!WARN! The outputs of this UI is very long! Do you want to continue? (y or n)") == "y":
                 pass
 
     class CUI_CMD():
@@ -37,6 +28,8 @@ class UI():
 
     class GUI():
         def __init__(self):
+            import tkinter
+            from tkinter import ttk
             self.root = tkinter.Tk()
             self.root.title("Marusoftware - Butterfly")
 
@@ -47,11 +40,27 @@ class UI():
 class CORE():
     def __init__(self, args=None):
         self.args = args
+        self.
         self._argparse()
+        if sys.version_info[0] == 3 or sys.version_info[1] >= 5:
+            print("Error: Python version is too old!¥nIf you want to continue, please use '--disable-pyvercheck'")
+            print(sys.version)
+            if not self.args.dispyvchk: sys.exit(1)
+        if self.args.version:
+            print(__version__)
+            sys.exit()
+        self._logging()
+    
     def _argparse(self):
-        #if self.args == None:
-        #    self.args = None
         self.arg_parser = argparse.ArgumentParser()
+        self.arg_parser.main = self.arg_parser.add_argument_group("MAIN")
+        parser = self.arg_parser.main
+        parser.add_argument("--disable-pyvercheck", action="store_true", dest="dispyvchk")
+        parser.add_argument("--version", action="store_true", dest="version")
+        self.arg_parser.log = self.arg_parser.add_argument_group("LOG")
+        parser.add_argument("-log_dir", action="store", type=str, dest="log_dir")
+        parser.add_argument("-log_path", action="store", type=str, dest="log_path")
+        parser.add_argument("-log_level", action="store_true", dest="log_level")
         self.arg_parser.ui = self.arg_parser.add_argument_group("UI")
         parser = self.arg_parser.ui 
         def arg_ui(i):
@@ -59,18 +68,21 @@ class CORE():
                 return list(i)
             else:
                 return str(i)
-        parser.add_argument("-ui", action="store", type=arg_ui)
+        parser.add_argument("-ui", action="store", type=arg_ui, dest="ui")
         parser.add_argument("--tui", action="store_true")
         parser.add_argument("--tui_p", action="store_true")
         parser.add_argument("--cui", action="store_true")
         parser.add_argument("--shell", action="store_true")
         parser.add_argument("--socket", action="store_true")
         parser.add_argument("--gui", action="store_false")
-        self.arg_parser.parse_args(self.args)
+        self.args = self.arg_parser.parse_args(self.args)
     def _logging(self):
-        self.logger
+        if self.args.log_path != None:
+            pass
+        logging.BasicConfig(format='%(levelname)s:%(asctime)s:%(name)s| ')
+        self.logger = logging.getLogger(__name__)
 
-class ENVIRON():        
+class ENVIRON():
     class Python2():
         pass
     class Python3():
---
+++ libbutterfly.py
@@ -0,0 +1,46 @@
+import os, sys
+
+__version__ = "beta0.0.1-pre"
+
+class Butterfly():
+    def __init__(self):
+        pass
+class Package():
+    def __init__(self):
+        self.sptxt = "."
+    def version(self, module):
+        module = module.split(self.sptxt)
+    def install(self, module):
+        module = module.split(self.sptxt)
+    def uninstall(self, module):
+        module = module.split(self.sptxt)
+    def addsource(self, source):
+        module = module.split(self.sptxt)
+    #def build(self, module):
+    #    module = module.split(self.sptxt)
+
+class Dir():
+    def __init__(self):
+        self.base_dir = ""
+    def get_module_Dir(self, module):
+        os.path.join(self.base_dir ,module_name)
+    def get_globalconf_Dir(self):
+        return 
+
+class Config():
+    def __init__(self, ftype, module, fmode="default"):
+        self.type = ftype
+        self.module = module
+        self._permition()
+        if ftype == "pickle":
+            pass
+        elif ftype=="ini":
+            pass
+        else:
+            raise TypeError
+    def setPermittion(self, to):
+        if 
+    def _permition(self):
+        return 1
+    class _Pickle():
+        self.config = open()
